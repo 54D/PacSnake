7d76bb2835cc1b462195e0bfbe73a1d0f68eff42
54D <linusworld@yahoo.com.hk>
2020-12-04 22:52:38 +0800

    Game view documentation

--- game_view.h
+++ game_view.h
@@ -24,20 +24,21 @@ class game_view : public QWidget
     Q_OBJECT
 
 public:
+
 	/**
-	 * Constructor for game_view
+	 * Constructor for game_view.
 	 * @param	parent			This constructor must accept its parent node as a parameter as specified by Qt.
 	 * @param	achievement		The achievement object to track the data.
-	*/
+	 */
 	game_view(QWidget *parent = nullptr, Achievement *achievement = nullptr);
 	/**
-	 * Destructor for game_view
-	*/
+	 * Destructor for game_view.
+	 */
 	~game_view();
 
 	/**
 	 * Public const. 
-	*/
+	 */
 	static const int GAME_TICK_UPDATE_TIME = 10;
 
 	static const int NUM_OF_NORMAL_GHOST = 4;
@@ -46,59 +47,162 @@ public:
 	static const int MAX_NUM_OF_POWERUP = 2;
 
 protected:
+
 	/**
 	 * Handler for key presses
-	 * @param	even	The event to process.
-	*/
+	 * @param	event	The event to process.
+	 */
     void keyPressEvent(QKeyEvent *event) override;
 
 private slots:
+
+	/**
+	 * Handler for `pushButton` which starts the game.
+	 */
     void on_pushButton_clicked();
+	/**
+	 * Handler for `pauseButton` which toggles pause status.
+	 */
     void on_pauseButton_clicked();
+	/**
+	 * Handler for `resetButton` which resets the game map.
+	 */
     void on_resetButton_clicked();
+	/**
+	 * Handler for `back_button` which brings `stackedWidget` back to main menu.
+	 */
     void on_back_button_clicked();
 	/**
 	 * Handler for `stackedWidget` switching to the game view.
 	 */
     void stackedWidgetChanged(int index);
+	/**
+	 * Refreshes the powerup inventory.
+	 * Called when there is a potential change in powerup inventory.
+	 */
     void refresh_powerUp_list();
+	/**
+	 * Updates the timer.
+	 * Called when the timer needs to be updated.
+	 */
 	void update_timer();
-    void update_health();
+	/**
+	 * Updates the health view.
+	 * Called when there is a potential change in health.
+	 */
+	void update_health();
+	/**
+	 * Updates the game map.
+	 * Called at every game tick advance.
+	 */
 	void gameTickUpdate();
+	/**
+	 * Handler for `volume_control` which toggles the visibility for volume slider.
+	 */
     void on_volume_control_clicked();
+	/**
+	 * Handler for `vol_Slider` which controls the volume of sound effects.
+	 */
     void on_vol_Slider_valueChanged(int value);
 
 signals:
-    void snake_collided(QList<QGraphicsItem*> collisions);
+
+	/**
+	 * Signal fired when previous menu navigation has been requested.
+	 */
     void previous_menu();
+	/**
+	 * Signal fired when the game over conditions are reached.
+	 */
 	void game_over_signal();
+	/**
+	 * Signal fired when a powerup is added to the inventory.
+	 */
 	void powerUp_added();
 
 private:
-    QGraphicsScene scene;
-    Ui::game_view *ui;
+
+	/**
+	 * Generates a random number with bounds `[lower,upper)`
+	 * @param	lower	Lower bound, inclusive.
+	 * @param	upper	Upper bound, exclusive.
+	 */
 	int random(int lower, int upper) const;
-    bool eventFilter(QObject*, QEvent*) override;
+	/**
+	 * Standard Qt Event Filter. Used to filter additional keypress events that may be consumed by child widgets.
+	 * @param	obj		Object captured.
+	 * @param	event	Event captured.
+	 */
+	bool eventFilter(QObject *obj, QEvent *event) override;
+	/**
+	 * Sets up the entire UI.
+	 */
     void setup_view();
+	/**
+	 * Resets the entire UI.
+	 */
     void reset_view();
+	/**
+	 * Populates the game area with images.
+	 */
     void render_game_map();
 
+	/**
+	 * Instantiates a powerup.
+	 */
 	void powerUp_instantiation();
+	/**
+	 * Instantiates a fruit.
+	 */
 	void fruit_instantiation();
+	/**
+	 * Instantiates a normal ghost.
+	 */
 	void normalGhost_instantiation();
+	/**
+	 * Instantiates a big ghost.
+	 */
 	void bigGhost_instantiation();
 
-	// Detect will the Entity collide with the wall or ghost on its next movement
+	/**
+	 * Detects whether an Entity will collide with the wall or a ghost on its next movement.
+	 * @param	row					Current row location.
+	 * @param	col					Current column location.
+	 * @param	headingDirection	Current heading direction.
+	 * @return	Prediction result.
+	 */
 	bool next_move_ghost_wall_collision(int row, int col, MovingEntity::Direction headingDirection) const;
-	// Detect will the Entity collide with the snake on its next movement
+	/**
+	 * Detects whether an Entity will collide with the snake on its next movement.
+	 * @param	row					Current row location.
+	 * @param	col					Current column location.
+	 * @param	headingDirection	Current heading direction.
+	 * @return	Prediction result.
+	 */
 	bool next_move_snake_collision(int row, int col, MovingEntity::Direction headingDirection) const;
-	// Detect will the Entity collide with the ghost on its next movemebt
+	/**
+	 * Detects whether an Entity will collide with a ghost on its next movement.
+	 * @param	row					Current row location.
+	 * @param	col					Current column location.
+	 * @param	headingDirection	Current heading direction.
+	 * @return	Prediction result.
+	 */
 	bool next_move_ghost_collision(int row, int col, MovingEntity::Direction headingDirection) const;
 
+	/**
+	 * Checks if the game over conditions have been reached.
+	 * @return	Whether the game is over.
+	 */
 	bool is_game_over() const;
+	/**
+	 * Removes content in the game view.
+	 */
 	void remove_game_content();
 
 	/* GAME CONTENT */
+	QGraphicsScene scene;
+	Ui::game_view *ui;
+
 	GameMap* game_map {nullptr};
 	Snake* snake {nullptr};
 	QList<NormalGhost*> normalGhosts;
@@ -114,7 +218,6 @@ private:
     long timeCount = 0;
     bool backButtonPressed = false;
 
-    //QGraphicsPixmapItem *snake_pixmap;
     QList<QGraphicsPixmapItem*> terrain_pixmaps;
 	QMediaPlayer* selectSoundEffect;
 	QMediaPlayer* eatSoundEffect;
@@ -122,8 +225,8 @@ private:
 	QMediaPlayer* deathSoundEffect;
 	QMediaPlayer* gameOverSoundEffect;
 
-    //achievement
     Stats* curr_stats;
+
 };
 
 #endif // GAME_VIEW_H
